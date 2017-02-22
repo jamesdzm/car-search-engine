ae2aaf4696e9701728fe975202f50d77e533854e
jamesdzm <j.dziewulskimiles@gmail.com>
2017-02-22 13:21:50 +0000

    Updates

--- App/src/Questions.js
+++ App/src/Questions.js
@@ -1,83 +1,35 @@
 import React from "react";
-import { Router, Route, Link, browserHistory } from "react-router";
-
-class QuestionOne extends React.Component {
-  render(){
-    return (
-      <div className="form-group">
-        <label><h1>What make and model do you want?</h1></label>
-        <input type="text" className="form-control" placeholder="Start typing make and model here" autoFocus={true} />
-        <Link to="/question-two">
-          <button className="btn btn-info">Next</button>
-        </Link>
-      </div>
-    )
-  }
-}
-
-class QuestionTwo extends React.Component {
-  render(){
-    return (
-      <div className="form-group">
-        <label>What age do you want?</label>
-        <input type="text" className="form-control" placeholder="Start typing age here" autoFocus={true} />
-        <Link to="/question-three">
-          <button className="btn btn-info">Next</button>
-        </Link>
-      </div>
-    )
-  }
-}
-
-class QuestionThree extends React.Component {
-  render(){
-    return (
-      <div className="form-group">
-        <label>What color do you want?</label>
-        <input type="text" className="form-control" placeholder="Start typing color here" autoFocus={true} />
-        <Link to="/question-four">
-          <button className="btn btn-info">Search</button>
-        </Link>
-      </div>
-    )
-  }
-}
-
-class QuestionFour extends React.Component {
-  render(){
-    return (
-      <div className="form-group">
-        <label>What is your postcode?</label>
-        <input type="text" className="form-control" placeholder="Enter postcode here" autoFocus={true} spellCheck={false} />
-        <Link to="/">
-          <button className="btn btn-info">Search</button>
-        </Link>
-      </div>
-    )
-  }
-}
 
 class Questions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { make: '', model: '', age: '', color: '', postcode: '' };
+
+  constructor(){
+    super();
+
+    this.state = {
+      updateCar: {}
+    }
   }
 
-  handleChange(e) {
-    this.setState({make: e.target.make, model: e.target.model, age: this.target.age, color: this.target.color, postcode: this.target.postcode })
+  handleSubmit(e){
+    this.setState({
+      updateCar: {
+        make: this.refs.make.value,
+      }
+    },
+    function() {
+      this.props.changeCar(this.state.updateCar);
+    });
+    e.preventDefault();
   }
 
   render(){
     return (
       <div className="question-container">
         <div className="question-inner">
-          <form>
-            <Router history={browserHistory}>
-              <Route path="/" component={QuestionOne} />
-              <Route path="question-two" component={QuestionTwo} />
-              <Route path="question-three" component={QuestionThree} />
-              <Route path="question-four" component={QuestionFour} />
-            </Router>
+          <form className="form" onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group">
+              <input type="text" className="form-control" ref="make" placeholder="Make" autoFocus={true} />
+            </div>
           </form>
         </div>
       </div>
--- App/src/app.js
+++ App/src/app.js
@@ -1,14 +1,46 @@
 import React from 'react';
-
-// import QuestionOne from './components/question-one';
 import Questions from './Questions';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      car: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({
+      car: [
+        {
+          postCode: "RH15 8HU",
+          make: "audi",
+          model: "a1",
+          startYear: "2010",
+          endYear: "2017",
+          colour: "white"
+        }
+      ]
+    })
+  }
+
+  handleChangeCar(newCar){
+    this.setState({
+      car: [
+        {
+          make: newCar.make
+        }
+      ]
+    }, function(){
+      console.log(this.state.car);
+    })
+  }
+
   render() {
     return (
       <div className="container-fluid">
-        {/* <QuestionOne /> */}
-        <Questions />
+        <Questions car={this.state.car} changeCar={this.handleChangeCar.bind(this)} />
       </div>
     )
   }
